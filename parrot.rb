97140819ed6bc8d0d54af380ae8910e7c7862db0
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-03-23 18:29:52 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(parrot = "Squawk!")
+puts parrot
+return parrot
+end
+
+
+
+def print_and_return_name
+  puts "Pretty bird!"
+end
